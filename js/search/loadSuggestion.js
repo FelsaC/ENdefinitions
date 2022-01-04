a55aab26082aec1bd60e9188ba748085ebe7a3db
Felipe <felsacor@gmail.com>
2022-01-04 07:24:44 -0300

    pequenas modificações de css e adicionado verificação extra em na função loadSuggestion

--- js/search/loadSuggestion.js
+++ js/search/loadSuggestion.js
@@ -1,17 +1,18 @@
 import { toggleSuggestion } from "./extra.js"
 
 export default async function loadSuggestion (input) {
-   fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-       method: "GET",
-       headers: {
-           "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-            "x-rapidapi-key": "bec3f1a7a8mshb9d8e1147d7b805p1d21d4jsn9f2f8cfd64c3"
-       }
-   })
-   .then(response => response.json()) 
-   .then(data => placeSuggestion(data, input))
-   .catch(err => console.log("error: ", err))
-
+    if(input.value == "") {
+        fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+                    "x-rapidapi-key": "bec3f1a7a8mshb9d8e1147d7b805p1d21d4jsn9f2f8cfd64c3"
+            }
+        })
+        .then(response => response.json()) 
+        .then(data => placeSuggestion(data, input))
+        .catch(err => console.log("error: ", err))
+    }
 }
 
 function placeSuggestion(data, input) {
